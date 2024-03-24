438c160dc97ce870d08e3113b6058ca1eeecc2a4
takamdev <panguslariss@gmail.com>
2024-03-24 10:34:03 +0100

    banier pour la pag boutique terminer

--- src/pages/Boutique.jsx
+++ src/pages/Boutique.jsx
@@ -4,22 +4,20 @@ function Boutique() {
    return (
       <section id="boutique" className="container-fluid">
          <div className="banier row">
-            <div className="col-md-6 col-sm-12 row ms-auto align-items-center">
-               <p className=" mb-0 pb-0">
-                  <span className="display-4 fw-bold">
-                     {" "}
-                     Un Large Choix <br /> De Cours
-                  </span>
-                  <br />
-                  <span className="fs-4">
+            <div className="col-md-6 col-sm-12 pt-5  align-items-center">
+               <p className="text-center display-4 mt-lg-5 fw-bold">
+               Un Large Choix <br /> De Cours
+               </p>
+               <p className="fs-5 text-center">
                      choisir 213000 videos de cours en ligne avec les mises a
                      jour nouvautés chaque mois
-                  </span> <br />
-                  <button type="button" className="btn btn-primary rounded-pill pt-3 pb-3 ps-3">Explorez Nos Cours</button>
-                  <button type="button" className="btn btn-outline-warning rounded-pill pt-3 pb-3 startCourse">Commencer le cours <span className="arrow"><i className="fa-solid fa-arrow-right"></i></span></button>
+                  </p>
+               <p className=" mt-5 d-flex justify-content-center">
+               <button type="button" className="btn btn-primary rounded-pill  pt-lg-3 pb-lg-3 ps-lg-3">Explorez Nos Cours</button>
+                  <button type="button" className="btn btn-outline-warning rounded-pill pt-lg-3 ms-3 pb-lg-3 startCourse">Commencer le cours <span className="arrows"><i className="fa-solid fa-arrow-right"></i></span></button>
                </p>
             </div>
-            <div className="col-6  d-sm-none d-md-block">
+            <div className="col-6 imgBanier d-md-block">
                <img
                   src="./images/baniere.png"
                   alt="image"
