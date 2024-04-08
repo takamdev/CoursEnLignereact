1abde432e055cae00be446fb75c9bdfe9d1e1765
takamdev <panguslariss@gmail.com>
2024-04-08 21:49:32 +0200

    amelioration de la navbar

--- src/components/NavBars.jsx
+++ src/components/NavBars.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 
 function NavBars() {
+
+  const hide = ()=>{
+    document.querySelector('#navbarSupportedContent').classList.remove('show')
+  }
   return (
     <nav id='navbar' className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
   <div className="container-fluid">
-    <Link className="navbar-brand ms-3" to="/"><img src="/images/logo.png" width={50} alt="image" /></Link>
+    <Link className="navbar-brand ms-3" onClick={hide} to="/"><img src="/images/logo.png" width={50} alt="image" /></Link>
    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <div className={`collapse navbar-collapse `} id="navbarSupportedContent">
     <form className="d-flex ms-lg-5" role="search">
         <input className="form-control me-2" type="search" placeholder="Nom Du Cours..." aria-label="Search"  />
         <i className='fa-solid fa-magnifying-glass search icoSearch'></i>
       </form>
       <ul className="navbar-nav ms-auto me-3 gap-lg-5 mb-2 mb-lg-0">
         <li className="nav-item">
-          <NavLink className="nav-link" aria-current="page"  to="/" >Boutique</NavLink>
+          <NavLink className="nav-link" onClick={hide} aria-current="page"  to="/" >Boutique</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/ListCours">Liste Cours</NavLink>
+          <NavLink className="nav-link" onClick={hide} to="/ListCours">Liste Cours</NavLink>
         </li>
         <li className="nav-item dropdown">
           <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Pages
           </Link>
           <ul className="dropdown-menu">
-            <li><Link className="dropdown-item" to="/Formateur">Formateurs</Link></li>
-            <li><Link className="dropdown-item" to="/Blog">Blog</Link></li>
+            <li><Link className="dropdown-item" onClick={hide} to="/Formateur">Formateurs</Link></li>
+            <li><Link className="dropdown-item" onClick={hide} to="/Blog">Blog</Link></li>
           </ul>
         </li>
         <li className="nav-item">
